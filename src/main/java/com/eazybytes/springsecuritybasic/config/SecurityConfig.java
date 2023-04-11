8fa71614e4952740ac0dd116aa4c6276f552ba9e
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-04-12 01:33:52 +0700

    custom spring security

---
+++ src/main/java/com/eazybytes/springsecuritybasic/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.eazybytes.springsecuritybasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        /**
+         *  Below is the custom security configurations
+         */
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/notices","/contact").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();
+    }
+
+}
